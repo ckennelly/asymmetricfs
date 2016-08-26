12c6d0e3618682622406526c8590765a90246c77
Chris Kennelly <ckennelly@ckennelly.com>
2016-08-25 23:48:33 -0700

    Add missing include.

--- src/memory_lock.cpp
+++ src/memory_lock.cpp
@@ -16,6 +16,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <cstddef>
 #include <boost/program_options.hpp>
 #include <ostream>
 #include "memory_lock.h"
